c452bcc6fbe9baa92d3d0e3db7c3b98b925f3c01
HikaruYabe <b1800744@planet.kanazawa-it.ac.jp>
2020-08-10 15:38:29 +0900

    細かい修正

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ from time import sleep
 
 def main():
     sensor_0 = sensor.DS18B20_sensor('/sys/bus/w1/devices/','/w1_slave')
-    print(sensor_0.read_temp())
-    sleep(1)
+    try:
+        while True:
+            print(sensor_0.read_temp())
+            sleep(1)
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == '__main__':
     main()
